16062278660847061d6aeaa7bc850f11c0153e28
Markus Wirsing <markus.wirsing@marcapo.com>
2016-09-02 13:55:21 +0200

    initial commit

---
+++ publish.sh
@@ -0,0 +1,4 @@
+cp ~/.atom/config.cson .
+cp ~/.atom/keymap.cson .
+cp ~/.atom/snippets.cson .
+apm list --installed --bare > packages.apm
---
+++ update.sh
@@ -0,0 +1,4 @@
+cp config.cson ~/.atom/config.cson
+cp keymap.cson ~/.atom/keymap.cson
+cp snippets.cson ~/.atom/snippets.cson
+apm install --packages-file packages.apm
