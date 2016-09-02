417491660cfba267b0613650c875ebb2d4603bc5
Markus Wirsing <markus.wirsing@marcapo.com>
2016-09-02 15:11:26 +0200

    added more plugins and styles

--- publish.sh
+++ publish.sh
@@ -1,4 +1,5 @@
 cp ~/.atom/config.cson .
 cp ~/.atom/keymap.cson .
 cp ~/.atom/snippets.cson .
+cp ~/.atom/styles.less .
 apm list --installed --bare > packages.apm
--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 cp config.cson ~/.atom/config.cson
 cp keymap.cson ~/.atom/keymap.cson
 cp snippets.cson ~/.atom/snippets.cson
+cp styles.less ~/.atom/styles.less
 apm install --packages-file packages.apm
